a2510cde2a3a47a2bfd908d5c4500bbaf538541b
Wilson Neto <eletricapuraque@gmail.com>
2025-04-09 17:21:26 -0300

    Primeiro commit

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import BatteryCalculator from "./BatteryCalculator";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BatteryCalculator />
+  </React.StrictMode>
+);
